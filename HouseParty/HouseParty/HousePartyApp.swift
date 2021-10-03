aa085addbea82ef78e0c321fd89d29e5b6111166
okuna <keithokuna@gmail.com>
2021-10-02 23:46:31 -0700

    blank iOS project

---
+++ HouseParty/HouseParty/HousePartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousePartyApp.swift
+//  HouseParty
+//
+//  Created by Keith Okuna on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HousePartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HouseParty/HousePartyTests/HousePartyTests.swift
@@ -0,0 +1,33 @@
+//
+//  HousePartyTests.swift
+//  HousePartyTests
+//
+//  Created by Keith Okuna on 10/2/21.
+//
+
+import XCTest
+@testable import HouseParty
+
+class HousePartyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
