5d93912fd80dc29b9bf1d75d67cf6d8b4d1acf39
alyssandra-cabading <alyssand@hawaii.edu>
2021-11-22 23:28:27 -1000

    ChecklistTasks

---
+++ HouseParty/HouseParty/Models/ChecklistTasks.swift
@@ -0,0 +1,16 @@
+//
+//  ChecklistTasks.swift
+//  HouseParty
+//
+//  Created by Alyssandra Cabading on 11/22/21.
+//
+
+import Foundation
+
+struct ChecklistTasks: Codable, Identifiable {
+    var id: String? { _id }
+    var _id: String?
+    var userId: String = ""
+    var taskName: String = ""
+    var notes: String = ""
+}
